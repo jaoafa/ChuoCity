bba804f9055ac65db9bf92b0ee1ca27f52df6aa5
Tomachi [ICHIGO] <lego@aol.jp>
2021-04-10 10:27:16 +0900

    fix: #1 TNT爆発を音とパーティクルのみに変更

--- src/main/java/com/jaoafa/Bakushinchi/Event/Event_PlaceTNT.java
+++ src/main/java/com/jaoafa/Bakushinchi/Event/Event_PlaceTNT.java
@@ -4,6 +4,8 @@ import com.jaoafa.Bakushinchi.Main;
 import com.jaoafa.Bakushinchi.PermissionsManager;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -26,10 +28,12 @@ public class Event_PlaceTNT implements Listener {
 
         String group = PermissionsManager.getPermissionMainGroup(player);
         if (!group.equalsIgnoreCase("Default") && !group.equalsIgnoreCase("Verified")
-                && !group.equalsIgnoreCase("Regular")) {
+            && !group.equalsIgnoreCase("Regular")) {
             return;
         }
         event.setCancelled(true);
-        block.getWorld().createExplosion(loc.getX(), loc.getY(), loc.getZ(), 4F, false, false);
+        block.getWorld().spawnParticle(Particle.EXPLOSION_LARGE, block.getLocation().add(0.5, 0.5, 0.5), 1);
+        block.getWorld().playSound(block.getLocation().add(0.5, 0.5, 0.5), Sound.ENTITY_GENERIC_EXPLODE, 1, 0.8f);
+        //block.getWorld().createExplosion(loc.getX(), loc.getY(), loc.getZ(), 4F, false, false);
     }
 }
