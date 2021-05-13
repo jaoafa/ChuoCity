03f22331aa6d6d9efe6ab47094dbd287e181092b
Tomachi [ICHIGO] <lego@aol.jp>
2021-05-14 01:55:17 +0900

    feat: #5 爆新地ないにおいて竹が伸びないように

--- src/main/java/com/jaoafa/Bakushinchi/Event/Event_Bamboo.java
+++ src/main/java/com/jaoafa/Bakushinchi/Event/Event_Bamboo.java
@@ -1,5 +1,6 @@
 package com.jaoafa.Bakushinchi.Event;
 
+import com.jaoafa.Bakushinchi.Main;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,8 +8,11 @@ import org.bukkit.event.block.BlockSpreadEvent;
 
 public class Event_Bamboo implements Listener {
     @EventHandler
-    public void onBlockSpreadEvent(BlockSpreadEvent event){
-        if(event.getBlock().getType() != Material.BAMBOO){
+    public void onBlockSpreadEvent(BlockSpreadEvent event) {
+        if (!Main.isBakushinchi(event.getBlock().getLocation())) {
+            return;
+        }
+        if (event.getSource().getType() != Material.BAMBOO && event.getSource().getType() != Material.BAMBOO_SAPLING) {
             return;
         }
         event.setCancelled(true);
