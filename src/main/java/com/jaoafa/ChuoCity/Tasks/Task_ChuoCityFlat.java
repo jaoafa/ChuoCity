c621ce1582d7f88f97ca4a7809062f5c7cef33f8
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-12-30 21:25:08 +0900

    fix: 中央市のフラットコマンドを修正

--- src/main/java/com/jaoafa/ChuoCity/Tasks/Task_ChuoCityFlat.java
+++ src/main/java/com/jaoafa/ChuoCity/Tasks/Task_ChuoCityFlat.java
@@ -31,10 +31,10 @@ public class Task_ChuoCityFlat extends BukkitRunnable {
                 player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "-> " + (set_0 ? "成功" : "失敗"));
             }
             case 2 -> {
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "岩盤範囲であるy-64～y-60を選択: //pos1,2 x,y,z");
+                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "岩盤範囲であるy-64を選択: //pos1,2 x,y,z");
                 boolean bedrock_pos1 = player.performCommand(String.format("/pos1 %d,%d,%d", region.getMinimumPoint().getBlockX(), -64, region.getMinimumPoint().getBlockZ()));
                 player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "pos1 -> " + (bedrock_pos1 ? "成功" : "失敗"));
-                boolean bedrock_pos2 = player.performCommand(String.format("/pos2 %d,%d,%d", region.getMaximumPoint().getBlockX(), -60, region.getMaximumPoint().getBlockZ()));
+                boolean bedrock_pos2 = player.performCommand(String.format("/pos2 %d,%d,%d", region.getMaximumPoint().getBlockX(), -64, region.getMaximumPoint().getBlockZ()));
                 player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "pos2 -> " + (bedrock_pos2 ? "成功" : "失敗"));
             }
             case 3 -> {
@@ -43,54 +43,30 @@ public class Task_ChuoCityFlat extends BukkitRunnable {
                 player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "-> " + (set_bedrock ? "成功" : "失敗"));
             }
             case 4 -> {
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "深層岩範囲であるy-59～y-1を選択: //pos1,2 x,y,z");
-                boolean stone_pos1 = player.performCommand(String.format("/pos1 %d,%d,%d", region.getMinimumPoint().getBlockX(), -59, region.getMinimumPoint().getBlockZ()));
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "pos1 -> " + (stone_pos1 ? "成功" : "失敗"));
-                boolean stone_pos2 = player.performCommand(String.format("/pos2 %d,%d,%d", region.getMaximumPoint().getBlockX(), -1, region.getMaximumPoint().getBlockZ()));
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "pos2 -> " + (stone_pos2 ? "成功" : "失敗"));
-            }
-            case 5 -> {
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "深層岩に変更: //set deepslate");
-                boolean set_deepslate = player.performCommand("/set deepslate");
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "-> " + (set_deepslate ? "成功" : "失敗"));
-            }
-            case 6 -> {
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "石範囲であるy0～y59を選択: //pos1,2 x,y,z");
-                boolean dirt_pos1 = player.performCommand(String.format("/pos1 %d,%d,%d", region.getMinimumPoint().getBlockX(), 0, region.getMinimumPoint().getBlockZ()));
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "pos1 -> " + (dirt_pos1 ? "成功" : "失敗"));
-                boolean dirt_pos2 = player.performCommand(String.format("/pos2 %d,%d,%d", region.getMaximumPoint().getBlockX(), 59, region.getMaximumPoint().getBlockZ()));
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "pos2 -> " + (dirt_pos2 ? "成功" : "失敗"));
-            }
-            case 7 -> {
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "石に変更: //set stone");
-                boolean set_stone = player.performCommand("/set stone");
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "-> " + (set_stone ? "成功" : "失敗"));
-            }
-            case 8 -> {
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "土範囲であるy63～y67を選択: //pos1,2 x,y,z");
-                boolean dirt_pos1 = player.performCommand(String.format("/pos1 %d,%d,%d", region.getMinimumPoint().getBlockX(), 60, region.getMinimumPoint().getBlockZ()));
+                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "石範囲であるy-63～y66を選択: //pos1,2 x,y,z");
+                boolean dirt_pos1 = player.performCommand(String.format("/pos1 %d,%d,%d", region.getMinimumPoint().getBlockX(), -63, region.getMinimumPoint().getBlockZ()));
                 player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "pos1 -> " + (dirt_pos1 ? "成功" : "失敗"));
                 boolean dirt_pos2 = player.performCommand(String.format("/pos2 %d,%d,%d", region.getMaximumPoint().getBlockX(), 66, region.getMaximumPoint().getBlockZ()));
                 player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "pos2 -> " + (dirt_pos2 ? "成功" : "失敗"));
             }
-            case 9 -> {
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "土に変更: //set dirt");
+            case 5 -> {
+                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "石に変更: //set stone");
+                boolean set_stone = player.performCommand("/set stone");
+                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "-> " + (set_stone ? "成功" : "失敗"));
+            }
+            case 6 -> {
+                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "土範囲であるy67～y67を選択: //pos1,2 x,y,z");
+                boolean dirt_pos1 = player.performCommand(String.format("/pos1 %d,%d,%d", region.getMinimumPoint().getBlockX(), 66, region.getMinimumPoint().getBlockZ()));
+                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "pos1 -> " + (dirt_pos1 ? "成功" : "失敗"));
+                boolean dirt_pos2 = player.performCommand(String.format("/pos2 %d,%d,%d", region.getMaximumPoint().getBlockX(), 66, region.getMaximumPoint().getBlockZ()));
+                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "pos2 -> " + (dirt_pos2 ? "成功" : "失敗"));
+            }
+            case 7 -> {
+                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "粗い土に変更: //set coarse_dirt");
                 boolean set_dirt = player.performCommand("/set dirt");
                 player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "-> " + (set_dirt ? "成功" : "失敗"));
             }
-            case 10 -> {
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "草ブロック範囲であるy67を選択: //pos1,2 x,y,z");
-                boolean dirt_pos1 = player.performCommand(String.format("/pos1 %d,%d,%d", region.getMinimumPoint().getBlockX(), 67, region.getMinimumPoint().getBlockZ()));
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "pos1 -> " + (dirt_pos1 ? "成功" : "失敗"));
-                boolean dirt_pos2 = player.performCommand(String.format("/pos2 %d,%d,%d", region.getMaximumPoint().getBlockX(), 67, region.getMaximumPoint().getBlockZ()));
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "pos2 -> " + (dirt_pos2 ? "成功" : "失敗"));
-            }
-            case 11 -> {
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "草ブロックに変更: //set grass_block");
-                boolean set_dirt = player.performCommand("/set grass_block");
-                player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "-> " + (set_dirt ? "成功" : "失敗"));
-            }
-            case 12 -> {
+            case 8 -> {
                 player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "選択範囲の解除: //sel");
                 boolean sel = player.performCommand("/sel");
                 player.sendMessage("[ChuoCity] " + ChatColor.GREEN + "-> " + (sel ? "成功" : "失敗"));
